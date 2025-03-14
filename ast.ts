9bf3be1bb057e70bd64abf313f6f27a7c6363db3
JordanPooleEffect <zmtabinas@gmail.com>
2025-03-14 16:27:51 +0800

    AST Ep2

--- ast.ts
+++ ast.ts
@@ -7,3 +7,31 @@ export type NodeType =
     | "UnaryExpr" 
     | "FunctionDeclaration";
 
+// Statements will not return a value in our program atleast
+export interface Stmt {
+    kind: NodeType;
+}
+
+export interface Program extends Stmt {
+    kind: "Program",
+    body: Stmt[];
+}
+
+export interface Expr extends Stmt {} 
+
+export interface BinaryExpr extends Expr { // Recursive data types
+    kind: "BinaryExpr"
+    left: Expr,
+    right: Expr,
+    operator: string;
+}
+
+export interface Identifier extends Expr {
+    kind: "Identifier",
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral",
+    value: number;
+}
