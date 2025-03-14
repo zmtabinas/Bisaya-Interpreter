1b226a964750dc7636bba231f723cefeea784633
JordanPooleEffect <zmtabinas@gmail.com>
2025-03-14 17:12:25 +0800

    AST | PARSER | BinaryExpr not in use yet

---
+++ PARSER/FRONTEND/ast.ts
@@ -0,0 +1,37 @@
+export type NodeType = 
+    | "Program" 
+    | "NumericLiteral" 
+    | "Identifier" 
+    | "BinaryExpr" 
+    | "CallExpr" 
+    | "UnaryExpr" 
+    | "FunctionDeclaration";
+
+// Statements will not return a value in our program atleast
+export interface Stmt {
+    kind: NodeType;
+}
+
+export interface Program extends Stmt {
+    kind: "Program",
+    body: Stmt[];
+}
+
+export interface Expr extends Stmt {} 
+
+export interface BinaryExpr extends Expr { // Recursive data types
+    kind: "BinaryExpr"
+    left: Expr,
+    right: Expr,
+    operator: string;
+}
+
+export interface Identifier extends Expr {
+    kind: "Identifier",
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral",
+    value: number;
+}
