65e93a801b4a20ea3df716ec159a005fea91f9d1
JordanPooleEffect <zmtabinas@gmail.com>
2025-03-19 07:48:15 +0800

    AdditiveExpr, MultiplicativeExpr, compatibility for % in lexer

--- PARSER/FRONTEND/parser.ts
+++ PARSER/FRONTEND/parser.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 import { Stmt, Program, Expr, BinaryExpr, NumericLiteral, Identifier } from "./ast.ts";
 import { tokenize, Token, TokenType } from "./lexer.ts";
 
@@ -14,6 +15,15 @@ export default class Parser {
 
     private eat () {
         const prev = this.tokens.shift() as Token;
+        return prev;
+    }
+
+    private expect (type: TokenType, err: any) {
+        const prev = this.tokens.shift() as Token;
+        if (!prev || prev.type != type) {
+            console.error("Pareser Error: \n", err, prev, "Expecting: ", type);
+            Deno.exit(1);
+        }
         return prev;
     }
 
@@ -37,8 +47,51 @@ export default class Parser {
         return this.parse_expr();
     }
 
+    // ==== Precidence ==== // !IMPORTANT!
+    // AssignmentExpr
+    // MemberExpr
+    // FunctionCall
+    // LogicalExpr
+    // ComparisonExpr
+    // AdditiveExpr
+    // MultiplicativeExpr
+    // PrimaryExpr
+
     private parse_expr (): Expr {
-        return this.parse_primary_expr();
+        return this.parse_additive_expr();
+    }
+
+    // (10 + 5) - 5 = 10 (left-hand precedence)
+    private parse_additive_expr (): Expr {
+        let left = this.parse_multiplicative_expr();
+
+        while(this.at().value == "+" || this.at().value == "-") {
+            const operator = this.eat().value;
+            const right = this.parse_multiplicative_expr();
+            left = {
+                kind: "BinaryExpr",
+                left,
+                right,
+                operator,
+            } as BinaryExpr;
+        }
+        return left;
+    }
+
+    private parse_multiplicative_expr(): Expr {
+        let left = this.parse_primary_expr();
+
+        while(this.at().value == "/" || this.at().value == "*" || this.at().value == "%") {
+            const operator = this.eat().value;
+            const right = this.parse_primary_expr();
+            left = {
+                kind: "BinaryExpr",
+                left,
+                right,
+                operator,
+            } as BinaryExpr;
+        }
+        return left;
     }
 
     private parse_primary_expr (): Expr {
@@ -50,6 +103,13 @@ export default class Parser {
             
             case TokenType.Number:
                 return { kind: "NumericLiteral", value: parseFloat(this.eat().value), } as NumericLiteral;
+
+            case TokenType.OpenParen: {
+                this.eat(); // eat the opening paren
+                const value = this.parse_expr();
+                this.expect(TokenType.CloseParen, "Unexpected token found inside parenthesized expression. Expected closing parenthesis"); // eat closing paren
+                return value;
+            }
 
             default:
                 console.error("Unexpected token found during parsing", this.at());
